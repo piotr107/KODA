355151716bf8d512c59422b24449a57db3cb78d9
kubeczki <comjakub@gmail.com>
2023-06-11 22:24:52 +0200

    upload

--- encoder.py
+++ encoder.py
@@ -25,21 +25,44 @@ def encode_golomb(n, m):
 
 
 def save_to_file(data):
-    with open('./encoded_data.data', 'wb') as filehandle:
+    with open('./encoded_data', 'wb') as filehandle:
         # Store the data as a binary data stream
         pickle.dump(data, filehandle)
 
-
-if __name__ == '__main__':
+def run_encode(filename, m):
     # load the image
-    img = cv2.imread('./barbara.pgm', 0)
+    img = cv2.imread('./'+filename, 0)
     # get binary representation of image as string
     binary_code = ''.join(np.unpackbits(img).astype(str))
     # compress binary code as length of zeros arrays
     compressed_code = list(map(len, binary_code.split('1')))
     # list of M values for map function
-    M_list = [M] * len(compressed_code)
+    M_list = [m] * len(compressed_code)
     # encode compressed code
     result = list(map(encode_golomb, compressed_code, M_list))
     # save tuple (M, result) to file
-    save_to_file([img.shape, M, result])
+    save_to_file([img.shape, m, result])
+
+    org_size = img.size
+    encoded_size = len(pickle.dumps(result, -1))
+    avg_length = len(pickle.dumps(result, -1))/len(result)
+    coding_ratio =encoded_size / org_size
+    return (org_size, encoded_size, avg_length, coding_ratio)
+
+def calc_avg_code_length(filename, m):
+    # load the image
+    img = cv2.imread('./'+filename, 0)
+    # get binary representation of image as string
+    binary_code = ''.join(np.unpackbits(img).astype(str))
+    # compress binary code as length of zeros arrays
+    compressed_code = list(map(len, binary_code.split('1')))
+    # list of M values for map function
+    M_list = [m] * len(compressed_code)
+    # encode compressed code
+    result = list(map(encode_golomb, compressed_code, M_list))
+    return len(pickle.dumps(result, -1))/len(result)
+
+
+if __name__ == '__main__':
+    result = run_encode('barbara.pgm', M)
+    print(result)
